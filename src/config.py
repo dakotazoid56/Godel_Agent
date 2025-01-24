29534d25e9f14de8be172f2e1916b831f2f03132
Dakota <dakotabarnes@ucsb.edu>
2025-01-23 23:49:16 -0800

    test all models from avior... no success on 'tool_calls'

--- src/config.py
+++ src/config.py
@@ -1,5 +1,7 @@
 import os
 import json
+import string
+import random
 from typing import Literal
 from openai import OpenAI
 from datetime import datetime
@@ -11,7 +13,6 @@ from datetime import datetime
 """
 ModelType = Literal["gpt-3.5-turbo", "gpt-4o-mini", "gpt-4o"]
 DEFAULT_MODEL: ModelType = "gpt-4o-mini"
-
 SOLVER_MODEL: Literal["gpt-3.5-turbo"] = "gpt-3.5-turbo"
 EVOLVE_MODEL: Literal["gpt-4o"] = "gpt-4o"
 def get_openai_instance():
@@ -22,25 +23,29 @@ def get_openai_instance():
 
 
 #USING AVIOR AI MODELS
-LLAMA_3_8B = "meta-llama/Meta-Llama-3.1-8B-Instruct"
-LLAMA_3_70B = "meta-llama/Llama-3.3-70B-Instruct"
-              
-ModelType = Literal[LLAMA_3_8B,LLAMA_3_70B]
-DEFAULT_MODEL: ModelType = LLAMA_3_8B
 
-SOLVER_MODEL: Literal[LLAMA_3_8B] = LLAMA_3_8B
+#Testing Done in tmp/run3
+N_LLAMA_31_8B = "nvidia/OpenMath2-Llama3.1-8B"                    # Doesnt work with api (BadRequestError)
+LLAMA_31_405B_I = "meta-llama/Meta-Llama-3.1-405B-Instruct-FP8"   # Output function name in message but not under "tool_calls" (Need another llm to match output to function?)
+LLAMA_33_70B_I = "meta-llama/Llama-3.3-70B-Instruct"              # Output whole function in message, no "tool_calls"
+LLAMA_32_3B_I = "meta-llama/Llama-3.2-3B-Instruct"                # Output whole function in message, no "tool_calls"
+LLAMA_31_8B_I = "meta-llama/Meta-Llama-3.1-8B-Instruct"           # Output whole function in message, no "tool_calls"
+LLAMA_31_8B = "meta-llama/Llama-3.1-8B"                           # Doesnt work with api (BadRequestError)
+QWEN_25_32_I = "Qwen/Qwen2.5-Coder-32B-Instruct"                  # Output whole function in message, no "tool_calls"
+QWEN_2_7_I = "Qwen/Qwen2-7B-Instruct"                             # Output whole function in message, no "tool_calls"
 
-EVOLVE_MODEL: Literal[LLAMA_3_70B] = LLAMA_3_70B
+#TESTING_MODEL = LLAMA_31_405B_I
+#EVOLVE_MODEL: Literal[TESTING_MODEL] = TESTING_MODEL
 
-
+ModelType = Literal[LLAMA_31_8B_I,LLAMA_33_70B_I]
+DEFAULT_MODEL: ModelType = LLAMA_31_8B_I
+SOLVER_MODEL: Literal[LLAMA_31_8B_I] = LLAMA_31_8B_I
+EVOLVE_MODEL: Literal[LLAMA_33_70B_I] = LLAMA_33_70B_I
 def get_openai_instance():
     # Add logic here if needed to create or configure the OpenAI client
     api_key = os.getenv("AVIOR_API_KEY")
     base = "http://avior.mlfoundry.com/live-inference/v1"
     return OpenAI(api_key=api_key, base_url=base)
-
-    
-
 
 
 
@@ -55,7 +60,14 @@ NON_SOLVER_MODELS = [
 def log_model_input_output(kwargs, response, model):
     # Get the current date and time for the filename
     current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    filename = f"../tmp/_{current_time}_{model.replace("/", "_")}.txt"
+    
+    # Get model name without "/" (in llama model name)
+    model_name = model.replace("/", "_")
+
+    # Add unique identifier at end... as there is many threads that spawn and run
+    unique_suffix = ''.join(random.choices(string.ascii_letters + string.digits, k=6))
+
+    filename = f"../tmp/run3/_{current_time}_{model_name}_{unique_suffix}.txt"
     
     # Ensure the directory exists
     os.makedirs(os.path.dirname(filename), exist_ok=True)
